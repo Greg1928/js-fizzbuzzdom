d422bf8384b3d5dd83d51f3e30f4fdb24bb16a1d
Greg1928 <gregoriosortino@protonmail.com>
2022-05-02 16:17:18 +0200

    added milestone2 and milestone3

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
+const list = document.querySelector(".list");
 /* from 1 to 100 */
 for (let i = 1; i<=100; i++){
+    const element = document.createElement("li");
+
+    element.classList.add("item");
+    element.innerText = i;
+    list.append(element)
     if((i%3 === 0) && (i%5 === 0)){
         console.log("FizzBuzz");
+        element.classList.add("both");
+        element.innerText = "FizzBuzz";
     }else if (i%5 === 0){
         console.log("Buzz");
+        element.classList.add("multiple-5");
+        element.innerText = "Buzz";
     }else if (i%3 === 0){
         console.log("Fizz");
+        element.innerText = "Fizz";
+        element.classList.add("multiple-3");
     }else{
         console.log(i);
+        element.classList.add("ordinary");
     }
     
 }
